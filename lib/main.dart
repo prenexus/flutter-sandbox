b25341a7ad89e08ce621efcd2678cbe9b5963cab
Mat Egan <mategan@gmail.com>
2018-06-03 03:52:13 +0100

    Sunday lunch

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
-import 'package:flutter/material.dart';
-import 'dart:io';
-import 'dart:async';
-import 'dart:convert' show utf8, json;
+// Issues :-
+// Reading preferences on startup doesnt refresh the screen and get the currencies correctly
+// Manual data entry isnt working
 
 //To do - list currencies
-// Query currencies from site
+// Query currencies from site on startup
 // Choose base currency
 // Choose conversion currency
+
+
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:convert' show utf8, json;
+import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 void main() {
   runApp(new MaterialApp(
@@ -19,12 +26,77 @@ class MyApp extends StatefulWidget {
   MyAppState createState() => new MyAppState();
 }
 
+class HomeCurrencyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Home Currency"),
+        ),
+        body:
+        new ListView(
+          children: <Widget>[
+            new ListTile(
+                title: new Text("AUD"),
+                onTap: () {
+    
+                }
+            )
+          ],
+        )
+    );
+
+
+  }
+}
+
+class SettingsPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context)
+  {
+    return new Scaffold(
+    appBar: new AppBar(
+        title: new Text("Settings"),
+      ),
+    body:
+    new ListView(
+            children: <Widget>[
+              new ListTile(
+                  leading: new Icon(Icons.attach_money),
+                  title: new Text ("Home Currency"),
+                onTap: ()
+                  {
+                     // Push currency list
+                      Navigator.push(
+                        context,
+                        new MaterialPageRoute(builder: (context) => new HomeCurrencyPage()),
+                      );
+                    },
+
+              ),
+              new ListTile(
+                  leading: new Icon(Icons.attach_money),
+                  title: new Text ("Away Currency"),
+                  onTap: ()
+                  {
+                    // Pick currency list
+                  }
+              ),
+            ],
+          ),
+    );
+
+  }
+}
+
+
 class MyAppState extends State<MyApp> {
   final _biggerFont = const TextStyle(fontSize: 24.0);
   double _exchangeRate = 1.0;
-  var _fromCurrency = 'AUD';
-  var _toCurrency = 'USD';
+  var _fromCurrency = 'AUD' ;
+  var _toCurrency  = 'AUD';
 
+  var _lastUpdated = new DateTime.now();
 
   var amount1 = 1;
   var amount2 = 10;
@@ -32,33 +104,50 @@ class MyAppState extends State<MyApp> {
   var amount4 = 50;
   var amount5 = 100;
 
+  //Future<Null>
+  _readPrefs() async {
+  // Open the shared preferences and find our currencies.
+     SharedPreferences prefs = await SharedPreferences.getInstance();
+    _fromCurrency = (prefs.getString('fromCurrency') ?? 'EUR');
+    _toCurrency = (prefs.getString('toCurrency')?? 'AUD');
+  }
+
+  _savePrefs(var homeOrAway, var currency) async {
+    // Save the preferences as required
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString(homeOrAway, currency);
+  }
+
   _getExchangeRate() async {
     // Get the exchange rate from free.currencyconverterapi.com
     // query = from_to . e.g AUD_USD
     // Returns a double value
-    var fromToCurrecy = _fromCurrency + "_" + _toCurrency;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _fromCurrency = (prefs.getString('fromCurrency') ?? 'EUR');
+    _toCurrency = (prefs.getString('toCurrency')?? 'AUD');
+
+    var fromToCurrency = _fromCurrency + "_" + _toCurrency;
 
     var url = 'https://free.currencyconverterapi.com/api/v5/convert?q=' +
-        fromToCurrecy;
+        fromToCurrency;
     var httpClient = new HttpClient();
 
-    var result;
     try {
       var request = await httpClient.getUrl(Uri.parse(url));
       var response = await request.close();
       if (response.statusCode == HttpStatus.OK) {
-        print('Status OK');
         var jsonString = await response.transform(utf8.decoder).join();
         print(jsonString);
         Map<String, dynamic> decodedMap = json.decode(jsonString);
-        _exchangeRate = decodedMap['results'][fromToCurrecy]['val'];
-        print(decodedMap['results'][fromToCurrecy]['val']);
+        _exchangeRate = decodedMap['results'][fromToCurrency]['val'];
+        print(decodedMap['results'][fromToCurrency]['val']);
+        _lastUpdated = new DateTime.now();
       } else {
-        result =
-        'Error getting API data address:\nHttp status ${response.statusCode}';
+        print ('Error getting API data address:\nHttp status ${response.statusCode}');
       }
     } catch (exception) {
-      result = 'Failed getting API data';
+      print ('Failed getting API data');
+      print (exception.toString());
     }
 
     // If the widget was removed from the tree while the message was in flight,
@@ -72,27 +161,41 @@ class MyAppState extends State<MyApp> {
     });
   }
 
+  //Run this at app startup
+  @override
+  void initState(){
+    super.initState();
+    _readPrefs();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold (
       appBar: new AppBar(
         title: new Text('Travel Currency Cheatsheet'),
         actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list),), //onPressed: null ),
-        ],
+          new IconButton(
+            icon: new Icon(Icons.settings),
+            onPressed: ()
+              {
+                Navigator.push(
+                  context,
+                  new MaterialPageRoute(builder: (context) => new SettingsPage()),
+                );
+              },
+            ),
+          ],
       ),
 
       body: _buildScreen(),
     );
   }
 
-  double _rate = 0.00;
-  double _usdamount = 100.00;
-
   void _onPressed() {
     setState(() {
       _exchangeRate = double.parse(_controller.text);
       print(_exchangeRate);
+      print ('Last updated is' + _lastUpdated.toString());
     }
     );
   }
@@ -115,8 +218,6 @@ class MyAppState extends State<MyApp> {
     //new Text('Caption', style: textTheme.caption),
     //new Text('BUTTON', style: textTheme.button),
 
-
-
     return new Container(
       padding: new EdgeInsets.all(32.0),
       child: new Column(
@@ -128,13 +229,13 @@ class MyAppState extends State<MyApp> {
               children: <Widget>[
                 new Column(
                   children: <Widget>[
-                    new Text('Home (' + _fromCurrency + ')',style: textTheme.headline),
+                    new Text( _fromCurrency ,style: textTheme.headline),
                   ],
                 ),
 
                 new Column(
                   children: <Widget>[
-                    new Text('Foreign (' + _toCurrency + ')', style: textTheme.headline),
+                    new Text( _toCurrency , style: textTheme.headline),
                   ],
                 )
 
@@ -173,13 +274,32 @@ class MyAppState extends State<MyApp> {
 
                 new Column(
                   children: <Widget>[
-                    new Text((amount1 *_exchangeRate).toStringAsFixed(2), style:_biggerFont),
+                    new Text((amount1 *_exchangeRate).toStringAsFixed(4), style:_biggerFont),
                   ],
                 )
 
               ],
 
             ),
+
+
+            new Row(
+              // Last update text
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+
+                new Text('Last updated: ' +
+                    _lastUpdated.day.toString() + '/' +
+                    _lastUpdated.month.toString() + '/' +
+                    _lastUpdated.year.toString() + ' ' +
+                    _lastUpdated.hour.toString() + ':' +
+                    _lastUpdated.minute.toString() + ' ' +
+                    _lastUpdated.timeZoneName.toString()
+                    ,textScaleFactor: .8),
+
+              ],
+            ),
+
 
             new Divider(height: 32.0, color: Colors.black),
 
